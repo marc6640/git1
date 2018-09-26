df32a24ff120624445d6bf9a26a9452008662b0d
Marcus Vikkelsøe Søndergaard Nielsen <marc6640@edu.eal.dk>
2018-09-26 09:31:16 +0200

    ændring i main på pc2

--- git1/Program.cs
+++ git1/Program.cs
@@ -11,7 +11,7 @@ namespace git1
         static void Main(string[] args)
         {
             
-            Console.WriteLine("Hello git1!");
+            Console.WriteLine("Hello git1 ændret på pc2!");
             Console.WriteLine("ny ændring");
             Console.ReadKey();
 
